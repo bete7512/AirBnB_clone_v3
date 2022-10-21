353c31475f0a3eea4d01e9b324b3c7dd140e18ed
bete7512 <betekbebe@gmail.com>
2022-10-21 20:21:04 +0300

    change commited succefully

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,10 +2,13 @@
 """"creating blue print for flask rest api routing"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', url_prefix='/api/v1')
+app_views = Blueprint('app_views',__name__, url_prefix='/api/v1')
 
 if app_views is not None:
     from api.v1.views.index import *
     # from api.v1.views.places import *
     # from api.v1.views.states import *
-    # from api.v1.views.users import *
+    from api.v1.views.cities import *
+    from api.v1.views.amenities import *
+    # from api.v1.views. import *
+    from api.v1.views.users import *
