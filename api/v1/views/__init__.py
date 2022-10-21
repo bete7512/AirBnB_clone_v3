0860c44d4c733be82304ebb15d606df2e7feed9e
bete7512 <betekbebe@gmail.com>
2022-10-21 11:42:41 +0300

    change commited succefully

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,4 +2,6 @@
 """"creating blue print for flask rest api routing"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', )
+app_views = Blueprint('app_views', url_prefix='/api/v1')
+
+if app_views is
