56ee949d8094b8f61d097c690151beb222734f56
bete7512 <betekbebe@gmail.com>
2022-10-21 20:10:56 +0300

    change commited succefully

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """"flask app starter"""
-from flask import Flask,make_response ,jsonify
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask_cors import CORS,cross_origin
+from flask_cors import CORS, cross_origin
 
-app=Flask(__name__)
-cors = CORS(app,resources={r"/api/*":{"origins":"*"}})
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def tear(self):
     ''''''''
     storage.close()
+
+
 @app.errorhandler(404)
 def path_not_found(error):
     ''''''''
-    return make_response(jsonify({'error':'Not found'},404))
+    return make_response(jsonify({'error': 'Not found'}, 404))
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,6 +6,6 @@ app_views = Blueprint('app_views', url_prefix='/api/v1')
 
 if app_views is not None:
     from api.v1.views.index import *
-    from api.v1.views.places import *
-    from api.v1.views.states import *
-    from api.v1.views.users import *
+    # from api.v1.views.places import *
+    # from api.v1.views.states import *
+    # from api.v1.views.users import *
