6d2261923ee119ffbf9a17786a71dacc934b40b9
bete7512 <betekbebe@gmail.com>
2022-10-21 19:45:38 +0300

    change commited succefully

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""""users"""
+from api.v1.views import app_views
+from flask import jsonify,abort,request
+from models import storage
+from models.user import User
+from datetime import datetime
+import uuid
+
+@app_views.route('/users/',methods=['GET'])
+def list_users():
+    """"list users """
+    list_users
