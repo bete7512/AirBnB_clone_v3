a7fe5848c375dcf3e241d254233190c817a779f3
bete7512 <betekbebe@gmail.com>
2022-10-21 12:54:11 +0300

    change commited succefully

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-"""ro"""
+"""first routes"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity 
+from models.review import Review
+
+classes = {"users":"User",
+            "cites":"City",
+            
+}
