1cbfcf1a9eac67c969dfa808a7cb59dfa69ad409
bete7512 <betekbebe@gmail.com>
2022-10-21 16:54:21 +0300

    change commited succefully

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """"cities fetching"""
+from datetime import datetime
 from api.v1.views import app_views
 
 from flask import jsonify,abort,request
 from models import storage
-from models.
+from models.city import City
+from models.state import State
+from datetime import datetime
+import uuid
+
+@app_views.route('/states')
