38ae7b548ffd0157afa0e6d4d2e03b9a9066b7bc
bete7512 <betekbebe@gmail.com>
2022-10-21 18:41:39 +0300

    change commited succefully

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,4 +11,9 @@ from datetime import datetime
 import uuid
 
 @app_views.route('/states/<state_id>/cities')
-def list_all_cities
+def list_all_cities(state_id):
+    all_states = storage.all("State").values()
+    current_state = [key.to_dict() for key in all_states if key.id == state_id]
+    if current_state == []:
+        abort(404)
+     all_cities = [key.to_dict() for key ] 
