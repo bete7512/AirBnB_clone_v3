35cd15589766e8461a9277a63464830f8c318a57
bete7512 <betekbebe@gmail.com>
2022-10-22 19:29:08 +0300

    change commited succefully

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -2,18 +2,21 @@
 """"users"""
 from crypt import methods
 from api.v1.views import app_views
-from flask import jsonify,abort,request
+from flask import jsonify, abort, request
 from models import storage
 from models.user import User
 from datetime import datetime
 import uuid
 
-@app_views.route('/users/',methods=['GET'])
+
+@app_views.route('/users/', methods=['GET'])
 def list_users():
     """"list users """
     users = [user.to_dict() for user in storage.all("User").values()]
     return jsonify(users)
-@app_views.route('/users/<user_id>',methods=['GET'])
+
+
+@app_views.route('/users/<user_id>', methods=['GET'])
 def get_user_by_id(user_id):
     """"fetch user data by id """
     users = storage.all("User").values()
@@ -21,7 +24,9 @@ def get_user_by_id(user_id):
     if user == []:
         abort(404)
     return jsonify(user)
-@app_views.route('/users/user_id>',methods=['DELETE'])
+
+
+@app_views.route('/users/user_id>', methods=['DELETE'])
 def delete_user_by_id(user_id):
     """"delete user by thier Id """
     users = storage.all("User").values()
@@ -34,10 +39,22 @@ def delete_user_by_id(user_id):
             storage.delete(user)
             storage.save()
             break
-    return jsonify({}),200
-@app_views('/users/',methods=['POST'])
+    return jsonify({}), 200
+
+
+@app_views('/users/', methods=['POST'])
 def add_new_user():
-    if not request.get_json():
-        abort(400,'Not a JSON')
-    if 'email' not in request.get_json():
-        abort(400,)
+      '''Creates a User'''
+      if not request.get_json():
+          abort(400, 'Not a JSON')
+      if 'email' not in request.get_json():
+          abort(400, 'Missing name')
+      if 'password' not in request.get_json():
+          abort(400, 'Missing name')
+      users = []
+      new_user = User(email=request.json['email'],
+                      password=request.json['password'])
+      storage.new(new_user)
+      storage.save()
+      users.append(new_user.to_dict())
+      return jsonify(users[0]), 201
