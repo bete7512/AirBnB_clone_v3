a2c682e2db7b44e75a97bfcc5ebb33483ae92298
bete7512 <betekbebe@gmail.com>
2022-10-22 18:53:41 +0300

    change commited succefully

--- test_get_count.py
+++ test_get_count.py
@@ -5,12 +5,4 @@ from models import storage
 from models.state import State
 
 print("All objects: {}".format(storage.count()))
-print("State objects: {}".format(storage.count('State')))
-
-first_state_id = list(storage.all(State).values())[0].id
-first_all_states = storage.all(State).values()
-state_obj = [obj.to_dict() for obj in first_all_states if obj.id == first_state_id]
-print(state_obj[0]['id'])
-single = storage.get('State', first_state_id)
-print("njdfvjk {}".format(single))
-print("First state: {}".format(state_obj))
+print("State objects: {}".format(storage.count(State)))
