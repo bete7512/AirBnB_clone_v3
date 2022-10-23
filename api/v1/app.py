4929afabad2f370b74088f379d682276fca4c1f7
bete7512 <betekbebe@gmail.com>
2022-10-23 19:43:49 +0300

    change commited succefully

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,7 @@ from os import getenv
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
