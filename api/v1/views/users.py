64dffd174653b27d2ca4dcfbf44de21f3afb6a3a
bete7512 <betekbebe@gmail.com>
2022-10-23 18:52:54 +0300

    change commited succefully

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -9,7 +9,6 @@ from datetime import datetime
 import uuid
 
 
-
 @app_views.route('/users', methods=['GET'])
 def list_users():
     """"list users """
@@ -39,11 +38,10 @@ def delete_user_by_id(user_id):
         if user.id == user_id:
             storage.delete(user)
             storage.save()
-            break
     return jsonify({}), 200
 
 
-@app_views.route('/users/', methods=['POST'])
+@app_views.route('/users', methods=['POST'])
 def add_new_user():
     '''Creates a User'''
     if not request.get_json():
@@ -61,3 +59,36 @@ def add_new_user():
     storage.save()
     users.append(new_user.to_dict())
     return jsonify(users[0]), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def updates_user(user_id):
+    '''Updates a User object'''
+    all_users = storage.all("User").values()
+    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
+    if user_obj == []:
+        abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    try:
+        user_obj[0]['first_name'] = request.json['first_name']
+    except:
+        pass
+    try:
+        user_obj[0]['last_name'] = request.json['last_name']
+    except:
+        pass
+    for obj in all_users:
+        if obj.id == user_id:
+            try:
+                if request.json['first_name'] is not None:
+                    obj.first_name = request.json['first_name']
+            except:
+                pass
+            try:
+                if request.json['last_name'] is not None:
+                    obj.last_name = request.json['last_name']
+            except:
+                pass
+    storage.save()
+    return jsonify(user_obj[0]), 200
